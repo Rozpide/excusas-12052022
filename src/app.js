125d4adde2107b676ed1dba67305c24250dcc061
Rozpi <joserozpide@yahoo.es>
2022-05-12 09:01:01 +0000

    nuevo-excusas-12052022

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// window.onload = function() {
+//write your code here
+let generadordeExcusas = () => {
+  let personaje = ["Batman", "Spiderman", "Superman", "Hulk"];
+  let verbo = [" aplasto", " rompio", " ensucio", " perdio"];
+  let algo = [" el coche", " el trabajo", " el portatil", " mi ropa"];
+  let lugar = [
+    " en la calle",
+    " en mi casa ",
+    " en el super",
+    " en la estación"
+  ];
+  let personajeelegido = Math.floor(Math.random() * personaje.length);
+  let verboelegido = Math.floor(Math.random() * verbo.length);
+  let algoelegido = Math.floor(Math.random() * algo.length);
+  let lugarelegido = Math.floor(Math.random() * lugar.length);
+
+  return (
+    personaje[personajeelegido] +
+    " " +
+    verbo[verboelegido] +
+    " " +
+    algo[algoelegido] +
+    " " +
+    lugar[lugarelegido]
+  );
 };
+window.onload = () => {
+  console.log("Hello Rigo from the console!");
+  document.querySelector(".btn").addEventListener("click", () => {
+    document.querySelector(".the-excuse").innerHTML = generadordeExcusas();
+  });
+};
+//   window.onload = () => {
+//     console.log("Hello Rigo from the console!");
+//     document.querySelector(".btn").addEventListener("click", () => {
+//       document.querySelector(".the-excuse").innerHTML = generadordeExcusas();
+//     });
+//   };
+
+//   console.log("Hello Rigo from the console!");
+// };
